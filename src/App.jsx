80a3018e1f2c84098b432894e2aa9644a8a9bae2
miracle <15924122287@139.com>
2026-04-17 23:55:43 +0800

    移除标题注释

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import './App.css'
 function App() {
   return (
     <div className="dashboard-container">
-      {/* 标题 */}
       <div className="header">
         <h1 className="title">全国各省产业现状</h1>
         <div className="subtitle">INDUSTRIAL DISTRIBUTION OF CHINA</div>
